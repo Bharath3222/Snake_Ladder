deab58f9fe35ef6082f8f33f6afd4accedb32135
Bharath3222 <bharat322.korada@gmail.com>
2022-07-03 18:23:55 +0530

    Die Rolled

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,5 +1,7 @@
 package org.BL.pp;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
@@ -7,6 +9,15 @@ public class SnakeAndLadder {
 		// uc1
 		int player = 1;
 		int startPosition = 0;
+		
+		//uc2
+		Random random = new Random();
+		int dieRoll = random.nextInt(7);
+		if(dieRoll==0)
+			dieRoll=1;
+		else
+			dieRoll=dieRoll;
+		System.out.println("Rolled Die number is :"+ dieRoll);
 	}
 	}
 
