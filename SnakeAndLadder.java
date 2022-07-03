89df14391522018fbebcd333dcbb53d6dcc36724
Bharath3222 <bharat322.korada@gmail.com>
2022-07-03 18:12:48 +0530

    Welcome to Snake and Ladder Programme contribution

---
+++ SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package org.BL.pp;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// uc1
+		int player = 1;
+		int startPosition = 0;
+	}
+	}
+
+
